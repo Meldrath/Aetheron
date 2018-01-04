f5d3cc661526a9901e9e8adab8092582c9fbfabf
Moonpatroller <mrjoshbgoldstein@gmail.com>
2018-01-03 21:25:35 -0800

    moving IO to Conn object

--- src/main/java/com/aetheron/Main.java
+++ src/main/java/com/aetheron/Main.java
@@ -5,22 +5,55 @@ import java.nio.channels.*;
 import java.nio.ByteBuffer;
 import java.util.function.Consumer;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 class Conn {
 
-    final AsynchronousSocketChannel channel;
-    final ByteBuffer buf;
-    Future<Integer> readFuture;
-    String input;
-    String output;
+    private final ByteBuffer buf = ByteBuffer.allocate(1024);
+    private final AsynchronousSocketChannel channel;
+    private final ConcurrentLinkedQueue<String> inputQueue = new ConcurrentLinkedQueue<>();
 
-    public Conn(AsynchronousSocketChannel channel, ByteBuffer buf) {
+    public Conn(AsynchronousSocketChannel channel) {
         this.channel = channel;
-        this.buf = buf;
-        readFuture = CompletableFuture.completedFuture(0);
+        readChannel();
+    }
+
+    public void sendOutput(String msg) {
+        try {
+            ByteBuffer buf = ByteBuffer.wrap(msg.getBytes());
+            while (buf.hasRemaining()) {
+                channel.write(buf).get();
+            }
+        } catch (Exception e) {
+            System.out.println("Error writing output: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public String getInput() {
+        return inputQueue.poll();
+    }
+
+    private void readChannel() {
+        channel.read(buf, null, new CompletionHandler<Integer, Object>() {
+            @Override
+            public void completed(Integer numRead, Object att) {
+                if (numRead > 0) {
+                    final byte[] receivedBytes = new byte[numRead];
+                    buf.flip();
+                    buf.get(receivedBytes);
+                    final String receivedString = new String(receivedBytes).trim();
+                    inputQueue.offer(receivedString);
+                    buf.clear();
+                    readChannel();
+                }
+            }
+
+            @Override
+            public void failed(Throwable ex, Object att) {
+                System.out.println("Failed to read channel: " + ex);
+            }
+        });
     }
 }
 
@@ -30,54 +63,25 @@ public class Main {
         final int port = 8060;
         final ConcurrentHashMap<Conn, Boolean> clientChannels = new ConcurrentHashMap<>();
 
-        final Consumer<AsynchronousSocketChannel> acceptHandler = new Consumer<AsynchronousSocketChannel>() {
-            @Override
-            public void accept(AsynchronousSocketChannel ch) {
+        final Consumer<AsynchronousSocketChannel> acceptHandler = ch -> {
 
-                System.out.println("Accepted a connection: " + ch);
+            System.out.println("Accepted a connection: " + ch);
 
-                ByteBuffer buf = ByteBuffer.allocate(50);
-                buf.put("Hello and goodbye.".getBytes());
-                buf.flip();
-                ch.write(buf);
+            ByteBuffer buf = ByteBuffer.allocate(50);
+            buf.put("Hello and goodbye.".getBytes());
+            buf.flip();
+            ch.write(buf);
 
-                clientChannels.put(new Conn(ch, ByteBuffer.allocate(1024)), false);
-            }
-
-            public void failed(Throwable exc, Object att) {
-                System.out.println("Failed to accept connection: " + exc);
-            }
+            clientChannels.put(new Conn(ch), false);
         };
 
         AsyncServer server = new AsyncServer(port, acceptHandler);
 
-        ByteBuffer buf = ByteBuffer.allocate(1024);
-
         while (true) {
             clientChannels.forEach((Conn conn, Boolean isBusy) -> {
-                if (conn.readFuture.isDone()) {
-                    try {
-                        int numRead = conn.readFuture.get();
-                        if (numRead > 0) {
-                            final byte[] receivedBytes = new byte[numRead];
-                            conn.buf.flip();
-                            conn.buf.get(receivedBytes);
-                            conn.input = new String(receivedBytes).trim();
-                            System.out.println("Input: " + conn.input);
-                            conn.buf.clear();
-                            
-                            clientChannels.forEach((Conn c, Boolean b) -> {
-                                c.channel.write(ByteBuffer.wrap(receivedBytes));
-                            });
-                            
-
-                        }
-
-                        // read again
-                        conn.readFuture = conn.channel.read(conn.buf);
-                    } catch (InterruptedException | ExecutionException e) {
-                        throw new RuntimeException(e);
-                    }
+                final String input = conn.getInput();
+                if (input != null) {
+                    clientChannels.forEach((Conn c, Boolean b) -> c.sendOutput(input));
                 }
             });
             // server.stop();
